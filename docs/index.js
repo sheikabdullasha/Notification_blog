99be7c5476b07985f67746c0eecbd066cb89d6d9
sheikabdullasha <49115356+sheikabdullasha@users.noreply.github.com>
2021-08-16 18:03:57 +0530

    Update index.js
    commented db

--- docs/index.js
+++ docs/index.js
@@ -38,14 +38,14 @@ async function init() {
 
   navigator.serviceWorker.addEventListener('message', event => {
     if (event.data === 'newData') {
-      showData();
+      //showData();
     }
   });
 
   const currentToken = await messaging.getToken();
   console.log("token is : ",currentToken);
-  fetch('/register', { method: 'post', body: currentToken });
-  showData();
+ // fetch('/register', { method: 'post', body: currentToken });
+  //showData();
 
   messaging.onTokenRefresh(async () => {
     console.log('token refreshed');
@@ -55,42 +55,42 @@ async function init() {
 
 }
 
-async function showData() {
-  const db = await getDb();
-  const tx = db.transaction('jokes', 'readonly');
-  const store = tx.objectStore('jokes');
-  store.getAll().onsuccess = e => showJokes(e.target.result);
-}
+// async function showData() {
+//   const db = await getDb();
+//   const tx = db.transaction('jokes', 'readonly');
+//   const store = tx.objectStore('jokes');
+//   store.getAll().onsuccess = e => showJokes(e.target.result);
+// }
 
-function showJokes(jokes) {
-  const table = document.getElementById('outTable');
+// function showJokes(jokes) {
+//   const table = document.getElementById('outTable');
 
-  jokes.sort((a, b) => parseInt(b.ts) - parseInt(a.ts));
-  const html = [];
-  jokes.forEach(j => {
-    const date = new Date(parseInt(j.ts));
-    html.push(`<div><div class="header">${date.toISOString()} ${j.id} (${j.seq})</div><div class="joke">${j.joke}</div></div>`);
-  });
-  table.innerHTML = html.join('');
-}
+//   jokes.sort((a, b) => parseInt(b.ts) - parseInt(a.ts));
+//   const html = [];
+//   jokes.forEach(j => {
+//     const date = new Date(parseInt(j.ts));
+//     html.push(`<div><div class="header">${date.toISOString()} ${j.id} (${j.seq})</div><div class="joke">${j.joke}</div></div>`);
+//   });
+//   table.innerHTML = html.join('');
+// }
 
-async function getDb() {
-  if (this.db) {
-    return Promise.resolve(this.db);
-  }
-  return new Promise(resolve => {
-    const openRequest = indexedDB.open("chuck", 1);
+// async function getDb() {
+//   if (this.db) {
+//     return Promise.resolve(this.db);
+//   }
+//   return new Promise(resolve => {
+//     const openRequest = indexedDB.open("chuck", 1);
 
-    openRequest.onupgradeneeded = event => {
-      const db = event.target.result;
-      db.createObjectStore('jokes', { keyPath: 'id' });
-    };
+//     openRequest.onupgradeneeded = event => {
+//       const db = event.target.result;
+//       db.createObjectStore('jokes', { keyPath: 'id' });
+//     };
 
-    openRequest.onsuccess = event => {
-      this.db = event.target.result;
-      resolve(this.db);
-    }
-  });
-}
+//     openRequest.onsuccess = event => {
+//       this.db = event.target.result;
+//       resolve(this.db);
+//     }
+//   });
+// }
 
 init();
